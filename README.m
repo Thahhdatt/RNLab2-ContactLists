417fdfcd49494362cc3b5a9b0e63679013f21bdd
Thahhdatt <140128761+Thahhdatt@users.noreply.github.com>
2024-04-24 13:16:33 +0700

    Update README.m

--- README.m
+++ README.m
@@ -1,5 +1,5 @@
-Tênh: Nguyễn Thành Đạt
+Tên: Nguyễn Thành Đạt
 
 Mssv: 2024802010138
 
-[﻿#lab2](https://github.com/Thahhdatt/RNLab2-ContactLists/assets/140128761/366880fd-580b-4ecf-9c86-2f5a67eddfc1)
+[#RNLab2-ContactLists](https://github.com/Thahhdatt/RNLab2-ContactLists/assets/140128761/366880fd-580b-4ecf-9c86-2f5a67eddfc1)
